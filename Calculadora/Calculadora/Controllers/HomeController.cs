c891dbd64d4aa6f052e3e15f43d56c1c0f8f4e3a
Francisco Melo <aluno23155@ipt.pt>
2022-03-22 14:48:19 +0000

    Numeros

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -13,8 +13,49 @@ namespace Calculadora.Controllers
             _logger = logger;
         }
 
+        [HttpGet] // este anotador é facultativo
         public IActionResult Index()
         {
+            //preparar os dados a serem enviados para a View na primeira intereção
+            ViewBag.Visor = "0";
+            return View();
+        }
+        
+
+        [HttpPost]  //so quando o formulario for submetido em 'post' ele será acionado
+        public IActionResult Index(string botao, string visor) {
+            //testar valor do 'botao'
+            
+            switch (botao)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    //pressionei um algarismo
+                    if(visor == "0") { visor = botao; }
+                    else { visor = visor + botao; }
+                    break;
+
+                case ",":
+                    //foi pressionado ','
+                    if (!visor.Contains(',')) visor += ',';
+                    break;
+
+                case "+/-":
+                    //vamos inverter o valor do 'visor'
+                    if (visor.Contains('-')) visor = visor.Substring(1);
+                    else visor = '-' + visor;
+                    break;
+            }
+            // preparar dados para serem enviados a View
+            ViewBag.Visor = visor;
             return View();
         }
 
